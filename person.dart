bb8278cdeb88ea2988931065c2fc7d17356026f9
BrenoItalo16 <contato@brenoitalo.com>
2024-12-08 15:57:45 -0300

    chore: Person use calculator

--- person.dart
+++ person.dart
@@ -1,4 +1,7 @@
-class Person {
+import 'calculator.dart';
+
+class Person extends Calculator {
+  //Método calcular IMC
   double imc(double weight, double hight) {
     //A variável "imc" recebe o cálculo
     double imc = weight / (hight * hight);
